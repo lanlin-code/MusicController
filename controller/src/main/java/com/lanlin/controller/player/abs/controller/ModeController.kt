d1a8f27d8f112e18eb5518e585f8b70ffaeb57bf
lanlin-code <2529813756@qq.com>
2022-03-24 21:21:03 +0800

    refactor: 重构ModeController接口及实现

--- controller/src/main/java/com/lanlin/controller/player/abs/controller/ModeController.kt
+++ controller/src/main/java/com/lanlin/controller/player/abs/controller/ModeController.kt
@@ -1,8 +1,11 @@
 package com.lanlin.controller.player.abs.controller
 
+import com.lanlin.controller.player.abs.MediaListenerManger
+
 interface ModeController {
     // 当前的播放模式
     var current: Int
+    val listenerManager: MediaListenerManger
     // 切换到下一个播放模式
     fun nextMode(): Int
     fun setMode(mode: Int)
--- controller/src/main/java/com/lanlin/controller/player/impl/controller/ModeControllerImpl.kt
+++ controller/src/main/java/com/lanlin/controller/player/impl/controller/ModeControllerImpl.kt
@@ -6,7 +6,7 @@ import com.lanlin.controller.player.abs.listener.ModeFilter
 import com.lanlin.controller.player.impl.MediaListenerMangerImpl
 import com.lanlin.controller.player.setting.MediaModeSetting
 
-class ModeControllerImpl(val listenerManager: MediaListenerManger = MediaListenerMangerImpl(),
+class ModeControllerImpl(override val listenerManager: MediaListenerManger = MediaListenerMangerImpl(),
                          override var current: Int
 ) : ModeController {
 
